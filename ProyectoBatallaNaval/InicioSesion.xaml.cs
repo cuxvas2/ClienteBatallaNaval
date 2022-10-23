29611499ae439729e891680fc574763c5ea88015
Victor <cuevas0122@gmail.com>
2022-10-23 16:27:58 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoBatallaNaval/InicioSesion.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoBatallaNaval
+{
+    public partial class InicioSesion : Window
+    {
+
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+        Registro registro = new Registro();
+
+        private void buttonIniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxCorreo.Text.Length == 0)
+            {
+                mensajeError.Text = "Escribe un Email.";
+                textBoxCorreo.Focus();
+            }
+            else if(!Regex.IsMatch(textBoxCorreo.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                mensajeError.Text = "Ingresa un Correo electronico valido.";
+                textBoxCorreo.Select(0, textBoxCorreo.Text.Length);
+                textBoxCorreo.Focus();
+            }
+            else
+            {
+                string correoElectronico = textBoxCorreo.Text;
+                string password = passwordBoxContraseña.Password;
+            }
+        }
+
+        private void buttonRegistrarse_Click(object sender, RoutedEventArgs e)
+        {
+            registro.Show();
+            Close();
+        }
+    }
+}
---
+++ ProyectoBatallaNaval/Registro.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoBatallaNaval
+{
+
+    public partial class Registro : Window
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+        private void Registrarse_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxCorreoElectronico.Text.Length == 0)
+            {
+                mensajeError.Text = "Escribe un correo electronico";
+                textBoxCorreoElectronico.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxCorreoElectronico.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                mensajeError.Text = "Ingresa un Correo electronico valido.";
+                textBoxCorreoElectronico.Select(0, textBoxCorreoElectronico.Text.Length);
+                textBoxCorreoElectronico.Focus();
+            }
+            else
+            {
+                string nombreUsuario = textBoxNombreUsuario.Text;
+                string correoElectronico = textBlockCorreoElectronico.Text;
+                string contraseña = passwordBoxContraseña.Password;
+                if (passwordBoxContraseña.Password.Length == 0)
+                {
+                    mensajeError.Text = "Escribe una contraseña";
+                    passwordBoxContraseña.Focus();
+                }
+                else if(passwordBoxConfirmarContraseña.Password.Length == 0)
+                {
+                    mensajeError.Text = "Confirma la contraseña. ";
+                    passwordBoxConfirmarContraseña.Focus();
+                }
+                else if(passwordBoxContraseña.Password != passwordBoxContraseña.Password)
+                {
+                    mensajeError.Text = "La contraseñas deben ser las mismas.";
+                    passwordBoxConfirmarContraseña.Focus();
+                }
+            }
+        }
+
+        private void Borrar_Click(object sender, RoutedEventArgs e)
+        {
+            Reiniciar();
+        }
+        public void Reiniciar()
+        {
+            textBoxCorreoElectronico.Text = "";
+            textBoxNombreUsuario.Text = "";
+            passwordBoxContraseña.Password = "";
+            passwordBoxConfirmarContraseña.Password = "";
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void InicioSesion_Click(object sender, RoutedEventArgs e)
+        {
+            InicioSesion inicioSesion = new InicioSesion();
+            inicioSesion.Show();
+            Close();
+        }
+    }
+}
